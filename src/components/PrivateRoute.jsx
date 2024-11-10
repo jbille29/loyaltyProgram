37c19220619bc10d7dc1347001aee72ff36e40a0
jbille29 <jbille29@gmail.com>
2024-11-10 17:32:42 -0600

    private route uses redux state

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,12 @@
 // src/components/PrivateRoute.jsx
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('token');
-  return token ? children : <Navigate to="/login" />;
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
